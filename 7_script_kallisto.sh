cc0a10c9f02e66fc405cf9f147fdacedcf583b81
Trantry <165653199+Trantry@users.noreply.github.com>
2024-05-09 21:29:57 +0200

    Update 7_script_kallisto.sh

--- 7_script_kallisto.sh
+++ 7_script_kallisto.sh
@@ -4,19 +4,19 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100G
-#SBATCH --time=2-00:00:00 ###
-#SBATCH --partition=E5
+#SBATCH --time=0-01:00:00 ###
+#SBATCH --partition=Lake
 #SBATCH --output=/home/tbessonn/stdout/%A_%a.out # standard output file format
 #SBATCH --error=/home/tbessonn/stderr/%A_%a.err # error file format
 
+cd /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/8_post_assembly_QC/1.5_complete-transcriptome/1_kallisto/
+
 #create index pour kallisto
-#kallisto index -i /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/7_kallisto/Genome_ref/tz_transcripts.idx /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/7_kallisto/Genome_ref/tzet_transcriptome_trinityV2_kraken2.Trinity.fasta
+kallisto index -i /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/8_post_assembly_QC/1.5_complete-transcriptome/1_kallisto/Genome_ref/tz_transcripts.idx /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/8_post_assembly_QC/1.5_complete-transcriptome/1_kallisto/Genome_ref/trinity_tzet_complete_transcriptome.Trinity.fasta
 
+cd /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/8_post_assembly_QC/1.5_complete-transcriptome/1_kallisto/
 
-
-cd /home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/7_kallisto/
-
-PARAMS=(/home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/7_kallisto/unclassified_sormeRNA_after_kraken2/*fwd.fq)
+PARAMS=(/home/tbessonn/Tetraripis_zetteli/tetraripis_zetteli_leg_transcriptome_novogene/8_post_assembly_QC/1.5_complete-transcriptome/1_kallisto/unclassified_sormeRNA_after_kraken2/*fwd.fq)
 f1="${PARAMS[$SLURM_ARRAY_TASK_ID]}"
 f2=${PARAMS[$SLURM_ARRAY_TASK_ID]%%fwd.fq}"rev.fq"
 name=$(basename "${PARAMS[$SLURM_ARRAY_TASK_ID]}" "fwd.fq") 
@@ -29,5 +29,4 @@ kallisto quant \
      $f1 \
      $f2 \
      -b 100 
-
 

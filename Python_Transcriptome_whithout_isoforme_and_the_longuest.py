5450d3dd4bc3a91fcbca27e07d216444e5f049d7
Trantry <165653199+Trantry@users.noreply.github.com>
2024-05-15 13:52:45 +0200

    Update Python_Transcriptome_whithout_isoforme_and_the_longuest.py

--- Python_Transcriptome_whithout_isoforme_and_the_longuest.py
+++ Python_Transcriptome_whithout_isoforme_and_the_longuest.py
@@ -1,6 +1,7 @@
 #source path/to/venv/bin/activate
 
 from Bio import SeqIO
+import re
 
 def filter_longest_isoforms(input_path, output_path):
     record_dict = SeqIO.to_dict(SeqIO.parse(input_path, "fasta"))
@@ -10,6 +11,11 @@ def filter_longest_isoforms(input_path, output_path):
         gene_id = "_".join(record_id.split("_")[:-1])
         if gene_id not in longest_isoforms or len(record.seq) > len(longest_isoforms[gene_id].seq):
             longest_isoforms[gene_id] = record
+
+    # Modifier les identifiants de séquence
+    for record_id, record in longest_isoforms.items():
+        record.id = re.sub(r"_i\d+$", "", record.id)
+        record.description = record.id
 
     with open(output_path, "w") as output_file:
         SeqIO.write(longest_isoforms.values(), output_file, "fasta")

daaeb0f302af05e444f115321e90660b713792a2
Trantry <165653199+Trantry@users.noreply.github.com>
2024-05-28 10:47:50 +0200

    Update 8_Script_DESeq2.R

--- 8_Script_DESeq2.R
+++ 8_Script_DESeq2.R
@@ -23,6 +23,11 @@ citation("apeglm")
 citation("pheatmap")
 citation("ggplot2")
 citation("dplyr")
+citation("EnhancedVolcano")
+citation("vsn")
+citation("BiocManager")
+
+
 setwd("/Users/trystan.bessonnier/Desktop/DESeq2_Transcriptome/Control_Deseq")
 ##########################################
 ## Import data & sample preparation ######
@@ -50,8 +55,7 @@ tx2gene <- tx2gene[, c("transcript", "gene")]
 #read in kallisto abundance files, summarizing by gene
 txi <- tximport(files, type = "kallisto", tx2gene = tx2gene)
 names(txi)
-#txi <- na.omit(txi) 
-#txi
+
 
 #Optionally, if you want to save this count matrix as a file
 #write.csv(assay(ddsMatrix), file="genecounts.raw.csv", quote=FALSE)
@@ -80,7 +84,7 @@ ddsMatrix<- estimateSizeFactors(ddsMatrix)
 # Calculer le count moyen pour chaque gène
 meanCounts <- rowMeans(counts(ddsMatrix))
 
-# Filtrer les gènes qui ont un count moyen supérieur à 10
+# Filtrer les gènes qui ont un count moyen supérieur à 5
 ddsMatrix <- ddsMatrix[meanCounts > 5, ]
 
 
@@ -184,14 +188,14 @@ gene_ids <- data.frame(Gene_ID = row.names(resLFCSig))
 # Afficher le tableau
 print(gene_ids)
 # Écrire le tableau dans un fichier .txt 
-write.table(gene_ids, file = "Candidate_shrinkLFC_genes_3743_pvalue_0.01.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
+#write.table(gene_ids, file = "Candidate_shrinkLFC_genes_3743_pvalue_0.01.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
 
 sum(resLFCSig$log2FoldChange>1)
 #926
 # Créer un tableau avec les gènes surexprimés (LFC > 0)
 overexpressed_genes <- subset(resLFCSig, log2FoldChange > 1)
 overexpressed_gene_ids <- data.frame(Gene_ID = row.names(overexpressed_genes))
-write.table(overexpressed_gene_ids, file = "overexpressed_shrink_LFC_gene_926_ids.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
+#write.table(overexpressed_gene_ids, file = "overexpressed_shrink_LFC_gene_926_ids.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
 
 
 sum(resLFCSig$log2FoldChange<(-1))
@@ -201,20 +205,21 @@ resLFCSig <- subset(resLFC, padj < 0.01)
 # Créer un tableau avec les gènes sous-exprimés (LFC < 0)
 underexpressed_genes <- subset(resLFCSig, log2FoldChange < (-1))
 underexpressed_gene_ids <- data.frame(Gene_ID = row.names(underexpressed_genes))
-write.table(underexpressed_gene_ids, file = "underexpressed_shrink_LFC_gene_2817_ids.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
+#write.table(underexpressed_gene_ids, file = "underexpressed_shrink_LFC_gene_2817_ids.txt", sep = "\t", row.names = FALSE, quote = FALSE, col.names = FALSE)
 
 
 ######################
 ######### PCA ########
 ######################
 #by default, plotPCA uses the top 500 most variable genes, can change with ntop=argument
-
-pcaData <- plotPCA(ntd, intgroup=c("condition","replicat"), returnData=TRUE,ntop=7000)
+library(ggplot2)
+pcaData <- plotPCA(rld, intgroup=c("condition","replicat"), returnData=TRUE,ntop=80000)
 pcaData["leg"]<- c("L1","L1","L1","L1","L2","L2","L2","L2","L3","L3","L3","L3")
 percentVar <- round(100 * attr(pcaData, "percentVar"))
 str(pcaData)
 #PCA 1 vs 2
-pca<-ggplot(pcaData, aes(PC1, PC2, color=leg)) +
+tkt<-pca<-ggplot(pcaData, aes(PC1, PC2, color=leg)) +
+  ggtitle("PCA des échantillons")+
   geom_point(size=0) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
@@ -224,6 +229,7 @@ pca<-ggplot(pcaData, aes(PC1, PC2, color=leg)) +
 coord_fixed()
 pca
 
+ggsave(filename = "Beautiful_PCA.png", plot = tkt, width = 10, height = 10, dpi = 300)
 
 ##################
 #### HEATMAP #####
@@ -231,13 +237,14 @@ pca
 ##################
 library("pheatmap")
 select <- order(rowMeans(counts(dds,normalized=TRUE)),
-                decreasing=TRUE)[1:6192] #Play with the value (gene number)
+                decreasing=TRUE)[1:35000] #Play with the value (gene number)
 df <- as.data.frame(colData(dds)[,c("condition","replicat")])
 rownames(df) <- colnames(dds)
-pheatmap(assay(ntd)[select,],color=colorRampPalette(c("blue", "white", "red"))(100000), 
-         cluster_rows=TRUE, show_rownames=F,scale = "row",
+gg<-pheatmap(assay(ntd)[select,],color=colorRampPalette(c("blue", "white", "red"))(35000), 
+         cluster_rows=TRUE, show_rownames=F,scale = "row",main="Heatmap des counts normalisés pour 35000 gènes entres les différents réplicats",
          cluster_cols=F, annotation_col=df)
 
+ggsave(filename = "Count_matrix.png", plot = gg, width = 10, height = 10, dpi = 300)
 ###########################
 ########## HEATMAP ########
 ## intra sample distance ##
@@ -259,22 +266,32 @@ pheatmap(ntd_cor)
 ##################
 #### MA-PLOT #####
 ##################
+DESeq2::plotMA(res,ylim=c(-25,25),main="condition_Fan_vs_Arolium", alpha = 0.01,
+               colLine = "grey40",  
+               colNonSig = "gray60",
+               colSig = "blue")
+
+
 #more useful to visualize the MA-plot for the shrunken log2 fold changes, which remove the noise associated with log2 fold changes from low count genes without requiring arbitrary filtering thresholds.
-DESeq2::plotMA(resLFC,ylim=c(-13,13),main="condition_Fan_vs_Arolium",alpha = 0.01, (resLFC$log2FoldChange < -1 | resLFC$log2FoldChange > 1 ),
+png(filename = "PlotMA_apelgm.png", width = 10, height = 10, units = "in", res = 300)
+DESeq2::plotMA(resLFC,ylim=c(-15,15),main="condition_Fan_vs_Arolium",alpha = 0.01, #(resLFC$log2FoldChange < -1 | resLFC$log2FoldChange > 1 ),
                colLine = "grey40",  
                colNonSig = "gray60",
                colSig = "blue")
-abline(h=c(-0.05,0.05), col="dodgerblue", lwd=2)
-#saved as "process_DEseq2_M.GoS_vs_F.GoS_plotMA_resLFC.tiff"
 
-DESeq2::plotMA(res,ylim=c(-30,30),main="condition_Fan_vs_Arolium", alpha = 0.01,
+dev.off()
+
+resLFCashr <- lfcShrink(dds, coef="condition_F_vs_A", type="ashr")
+DESeq2::plotMA(resLFCashr,ylim=c(-15,15),main="condition_Fan_vs_Arolium",alpha = 0.01,
                colLine = "grey40",  
                colNonSig = "gray60",
                colSig = "blue")
-abline(h=c(-0.01,0.01), col="dodgerblue", lwd=2)
-#saved as "process_DEseq2_M.GoS_vs_F.GoS_plotMA_res.tiff"
-#650x550
 
+resnormal <- lfcShrink(dds, coef="condition_F_vs_A", type="normal")
+DESeq2::plotMA(resnormal,ylim=c(-15,15),main="condition_Fan_vs_Arolium",alpha = 0.01,
+               colLine = "grey40",  
+               colNonSig = "gray60",
+               colSig = "blue")
 ######################################
 ## Heat Map all significant genes ####
 ######################################
@@ -293,7 +310,7 @@ rownames(norm_sig) <- norm_sig$gene
 # Enlevez la colonne des noms de gènes des données de la heatmap
 heatmap_data <- norm_sig[,2:13]
 # Créez la heatmap
-pheatmap(heatmap_data, 
+beautiful<-pheatmap(heatmap_data, 
          color=colorRampPalette(c("blue", "white", "red"))(10000),
          cluster_rows = T, 
          show_rownames = F, # Affiche les noms des gènes
@@ -301,26 +318,61 @@ pheatmap(heatmap_data,
          fontsize = 10, 
          scale = "row", 
          fontsize_row = 10, 
-         height = 20)
-#saved as "WJ-3820_DEseq2_L3.M.P_vs_R_heatmap_padj_0.05_L2FC_0.58.tiff"
-#1500x1000
+         height = 20,
+         main="Heatmap des 3743 gènes significativement différentiellement exprimés")
+
+ggsave(filename = "Heatmap_all_signi_genes_3743.png", plot = beautiful, width = 10, height = 10, dpi = 300)
 
 
 #####################
 ## Volcanot plot ####
 #####################
-
 BiocManager::install('EnhancedVolcano')
 BiocManager::install("ashr")
 library(EnhancedVolcano)
 
+res <- DESeq(dds)
 
-EnhancedVolcano(res,
-                lab = rownames(res),
-                x = 'log2FoldChange',
-                y = 'pvalue')
+# Effectuer le shrinkage des log fold changes
+res <- lfcShrink(dds, coef="condition_F_vs_A", type='apeglm')
 
-EnhancedVolcano(resLFC,
-                lab = rownames(resLFC),
-                x = 'log2FoldChange',
-                y = 'pvalue',)
+# Vérifiez les colonnes disponibles dans res pour confirmer la présence de padj
+head(res)
+
+# Renommer le gène Trinity
+rownames(res)[rownames(res) == "TRINITY_DN158453_c0_g1"] <- "UBX"
+rownames(res)[rownames(res) == "TRINITY_DN4045_c0_g1"] <- "mogsha"
+rownames(res)[rownames(res) == "TRINITY_DN9986_c0_g2"] <- "SCR"
+rownames(res)[rownames(res) == "TRINITY_DN1658_c0_g1"] <- "R-ant-yellow"
+
+# Générer le volcano plot avec le gène renommé
+super <- EnhancedVolcano(res,
+                         lab = rownames(res),
+                         x = 'log2FoldChange',
+                         y = 'padj',
+                         title = 'Volcano Plot: Condition Fan vs Arolium',
+                         subtitle = "Highlighting significant genes with log2FC < -1 | > 1  and p-adj < 0.01",
+                         pCutoff = 0.01,
+                         FCcutoff = 1,
+                         pointSize = 2.0,
+                         labSize = 4.0,
+                         col = c("grey30", "forestgreen", "royalblue", "red2"),
+                         legendLabels=c('NS','Log2FC','p-adj','p-adj & Log2FC'),
+                         legendPosition = 'top',
+                         legendLabSize = 10,
+                         legendIconSize = 3.0,
+                         drawConnectors = TRUE,
+                         widthConnectors = 0.5,
+                         colConnectors = 'grey30',
+                         max.overlaps = 100,
+                         selectLab = c('UBX', 'mogsha', 'SCR','R-ant-yellow'), # Utilisation du nouveau nom
+                         labCol = 'black',
+                         labFace = 'bold',
+                         boxedLabels = TRUE)
+
+# Afficher le volcano plot
+print(super)
+
+# Sauvegarder le volcano plot
+ggsave(filename = "VolcanoPlot_F_vs_A.png", plot = super, width = 10, height = 10, dpi = 300)
+
